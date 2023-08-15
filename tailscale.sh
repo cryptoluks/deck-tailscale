acb5483d8e3759d69a51dfcd062245e87287313b
cryptoluks <9020527+cryptoluks@users.noreply.github.com>
2023-08-15 12:27:17 +0200

    Update tailscale.sh

--- tailscale.sh
+++ tailscale.sh
@@ -1,94 +1,78 @@
 #!/usr/bin/env bash
 
-# make system configuration vars available
+set -euxo pipefail
+
+# Load system configuration variables.
 source /etc/os-release
 
-# set invocation settings for this script:
-# -e: Exit immediately if a command exits with a non-zero status.
-# -u: Treat unset variables as an error when substituting.
-# -o pipefail: the return value of a pipeline is the status of the last command to exit with a non-zero status, or zero if no command exited with a non-zero status
-set -eu -o pipefail
+# Create and switch to a temporary directory.
+temp_dir="$(mktemp -d)"
+trap "rm -rf ${temp_dir}" EXIT
+cd "${temp_dir}"
 
-# save the current directory silently
-pushd . > /dev/null
+# Fetch the latest Tailscale version.
+echo -n "Fetching Tailscale version..."
+tailscale_info="$(curl -s 'https://pkgs.tailscale.com/stable/?mode=json')"
+tarball_url="$(echo "$tailscale_info" | jq -r .Tarballs.amd64)"
+version="$(echo "${tarball_url}" | cut -d_ -f2)"
+echo "Version ${version}"
 
-# make a temporary directory, save the name, and move into it
-dir="$(mktemp -d)"
-cd "${dir}"
+# Download and extract the Tailscale package.
+echo -n "Downloading and extracting..."
+curl -sS "https://pkgs.tailscale.com/stable/${tarball_url}" | tar xzf -
+echo "Done."
 
-echo -n "Getting version..."
+extracted_dir="$(echo "${tarball}" | cut -d. -f1-3)"
+test -d "${extracted_dir}"
 
-# get info for the latest version of Tailscale
-tarball="$(curl -s 'https://pkgs.tailscale.com/stable/?mode=json' | jq -r .Tarballs.amd64)"
-version="$(echo ${tarball} | cut -d_ -f2)"
+# Set up target directory structure and organize files.
+install_dir="tailscale/usr"
+mkdir -p "${install_dir}/{bin,sbin,lib/{systemd/system,extension-release.d}}"
+cp -rf "${extracted_dir}/tailscale" "${install_dir}/bin/tailscale"
+cp -rf "${extracted_dir}/tailscaled" "${install_dir}/sbin/tailscaled"
 
-echo "got ${version}."
+# Create a systemd extension-release file.
+echo -e "SYSEXT_LEVEL=1.0\nID=steamos\nVERSION_ID=${VERSION_ID}" > "${install_dir}/lib/extension-release.d/extension-release.tailscale"
 
-echo -n "Downloading..."
+# Manage system extensions: create, remove old, install new.
+extensions_dir="/var/lib/extensions"
+mkdir -p "${extensions_dir}"
+rm -rf "${extensions_dir}/tailscale"
+cp -rf tailscale "${extensions_dir}/"
 
-# download the Tailscale package itself
-curl -s "https://pkgs.tailscale.com/stable/${tarball}" -o tailscale.tgz
+# Set up systemd service.
+cp -rf "${extracted_dir}/systemd/tailscaled.service" "/etc/systemd/system"
 
-echo "done."
+# Check and copy defaults if absent.
+defaults_path="/etc/default/tailscaled"
+[[ ! -f "${defaults_path}" ]] && cp -rf "${extracted_dir}/systemd/tailscaled.defaults" "${defaults_path}"
 
-echo -n "Installing..."
-
-# extract the tailscale binaries
-tar xzf tailscale.tgz
-tar_dir="$(echo ${tarball} | cut -d. -f1-3)"
-test -d $tar_dir
-
-# create our target directory structure
-mkdir -p tailscale/usr/{bin,sbin,lib/{systemd/system,extension-release.d}}
-
-# pull things into the right place in the target dir structure
-cp -rf $tar_dir/tailscale tailscale/usr/bin/tailscale
-cp -rf $tar_dir/tailscaled tailscale/usr/sbin/tailscaled
-
-# write a systemd extension-release file
-echo -e "SYSEXT_LEVEL=1.0\nID=steamos\nVERSION_ID=${VERSION_ID}" >> tailscale/usr/lib/extension-release.d/extension-release.tailscale
-
-# create the system extension folder if it doesn't already exist, remove the old version of our tailscale extension, and install our new one
-mkdir -p /var/lib/extensions
-rm -rf /var/lib/extensions/tailscale
-cp -rf tailscale /var/lib/extensions/
-
-# copy the systemd files into place
-cp -rf $tar_dir/systemd/tailscaled.service /etc/systemd/system
-
-# copy in the defaults file if it doesn't already exist
-if ! test -f /etc/default/tailscaled; then
-  cp -rf $tar_dir/systemd/tailscaled.defaults /etc/default/tailscaled
+# Handle overrides if absent.
+override_path="/etc/systemd/system/tailscaled.service.d/override.conf"
+if [[ ! -f "${override_path}" ]]; then
+  mkdir -p "$(dirname "${override_path}")"
+  cp -rf override.conf "${override_path}"
 fi
 
-# return to our original directory (silently) and clean up
-popd > /dev/null
-rm -rf "${dir}"
+echo "Tailscale installation complete. Managing services..."
 
-# copy in our overrides file if it doesn't already exist
-if ! test -f /etc/systemd/system/tailscaled.service.d/override.conf; then
-  mkdir -p /etc/systemd/system/tailscaled.service.d
-  cp -rf override.conf /etc/systemd/system/tailscaled.service.d/override.conf
-fi
-
-echo "done."
-
-echo "Starting services..."
-
+# Manage systemd-sysext service.
 if systemctl is-enabled --quiet systemd-sysext && systemctl is-active --quiet systemd-sysext; then
-  echo "systemd-sysext is already enabled and active"
+  echo "systemd-sysext is enabled and active."
 else
   systemctl enable systemd-sysext --now
 fi
 
+# Refresh and reload.
 systemd-sysext refresh > /dev/null 2>&1
 systemctl daemon-reload > /dev/null
 
+# Manage tailscaled service.
 if systemctl is-enabled --quiet tailscaled && systemctl is-active --quiet tailscaled; then
-  echo "tailscaled is already enabled and active; restarting it..."
+  echo "tailscaled is enabled and active. Restarting..."
   systemctl restart tailscaled
 else
   systemctl enable tailscaled --now
 fi
 
-echo "Tailscale installed and ready."
+echo "Tailscale setup complete."
